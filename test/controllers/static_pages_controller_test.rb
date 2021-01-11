d6d0175ab635ae3cde15d486096396fdb50c3d29
Nishanth <nishanthmathi628@gmail.com>
2021-01-12 00:00:24 +0530

    Use SSL and the Puma webserver in production

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -12,7 +12,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_path
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
     assert_select "title", "Home | #{@base_title}"
 
   end
